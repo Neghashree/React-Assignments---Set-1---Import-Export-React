1da2d23e9959ab300ed17a07369c5213cd2a415f
Neghashree <86755686+Neghashree@users.noreply.github.com>
2021-07-17 09:51:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-
+import Heading from ./src/components/Heading.js
 const App = () => {
   return (
     <div id="main">
